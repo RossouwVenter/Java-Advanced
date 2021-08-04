83b6858ef5debb4ef9a15e16a214d159a018cd33
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-04 07:22:58 +0200

    Interfaces

---
+++ OCP2_Exercise/src/main/java/Lesson6/SalesCalcs.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson6;
+
+/**
+ *
+ * @author rosso
+ */
+public interface SalesCalcs {
+    
+    public String getName();
+    public double calcSalesPrice();
+    public double calcCost();
+    public double calcProfit();
+    
+    
+}
